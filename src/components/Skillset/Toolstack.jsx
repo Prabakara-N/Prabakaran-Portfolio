75da98352d0b6a9218b03862e34acedfb4e614dc
prabakaran <mspk028@gmail.com>
2023-03-04 13:20:42 +0530

    add my portfolio

---
+++ src/components/Skillset/Toolstack.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiHeroku,
+  SiVercel,
+  SiNetlify,
+} from "react-icons/si";
+import { FaGitAlt } from "react-icons/fa";
+
+const Toolstack = () => {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaGitAlt />
+      </Col>
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+      </Col> */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNetlify />
+      </Col>
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <SiHeroku />
+      </Col> */}
+    </Row>
+  );
+};
+
+export default Toolstack;
