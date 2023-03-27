811c862c086e58775f39848c04f1162fe63baf69
prabakaran <mspk028@gmail.com>
2023-03-27 19:41:25 +0530

    food project added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,6 +7,7 @@ import netflix from "../assets/projects/netflix.jpg";
 import Ecommerce from "../assets/projects/e-commerce.jpg";
 import Expense_Tracker from "../assets/projects/expense-tracker.jpg";
 import weather from "../assets/projects/weather-api.jpg";
+import foodapp from "../assets/projects/food-app.png";
 
 const Projects = () => {
   return (
@@ -28,6 +29,17 @@ const Projects = () => {
               description="I just finished working on this shoe-shop website project using React JS and Tailwind CSS. I get pictures from Unsplash website, users can browse products, add items to their cart, and complete the checkout process. I used routing to make it easy for users to navigate between different pages, like the productm infomation and home pages. Plus, I also included local storage to store the user's shopping cart data, so they can resume their shopping session later.To manage state changes, I used React hooks like useContext, useState, and useEffect. These hooks helped me update the state of the website based on user interactions, like when a user adds or deletes items from their cart.I also added alert messages to notify users when they add an item to their cart, delete an item, or clear their cart."
               ghLink="https://github.com/Prabakara-N/react-shoe-cart"
               demoLink="https://prabakaran-shoestore-ecommerce.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={foodapp}
+              isBlog={false}
+              title="Food App"
+              description="I finished building a React food app! It uses Tailwind CSS for styling, and I implemented state management using useContext, useReducer, useState, and useEffect. I also integrated Firebase authentication, database, and storage into the app, so users can create accounts, store data, and access content securely.I used react-router-dom to handle navigation throughout the app. This made it easy to create multiple pages and ensure a seamless user experience. Additionally, I allowed the admin to add new food items and categorize them accordingly. When the admin adds a new item, the image is stored in Firebase storage. This ensures that the images are always available and load quickly for users. In addition, I stored all the cart items in local storage, so users can continue to view and manage their cart even if they leave the app and come back later."
+              ghLink="https://github.com/Prabakara-N/react-food-app"
+              demoLink="https://prabakaran-react-foodapp.netlify.app/"
             />
           </Col>
 
@@ -58,7 +70,7 @@ const Projects = () => {
               imgPath={Expense_Tracker}
               isBlog={false}
               title="Expense Tracker"
-              description="The Expense Tracker project is a simple yet powerful web application that allows users to keep track of their expenses. It was built using React JS and utilizes the useState and useEffect hooks. The project provides a responsive and user-friendly interface and includes features such as add, delete, edit, and view transactions, as well as a summary of the user's expenses and income. The alert notification feature provides a convenient way for users to confirm that their transactions have been added, edited, or deleted successfully.The project also includes two radio buttons, one for income and one for expenses. When the user selects the income radio button, the entered value will be added as a positive amount, and when the user selects the expense radio button, the entered value will be added as a negative amount. The use of the useEffect hook for storing transaction data in local storage and calculating changes in the transactions makes the application more efficient and user-friendly."
+              description="The Expense Tracker project is a simple yet powerful web application that allows users to keep track of their expenses. It was built using React JS and utilizes the useState and useEffect hooks. The project provides a responsive and user-friendly interface and includes features such as add, delete, edit, and view transactions, as well as a summary of the user's expenses and income. The alert notification feature provides a convenient way for users to confirm that their transactions have been added, edited, or deleted successfully. The use of the useEffect hook for storing transaction data in local storage and calculating changes in the transactions makes the application more efficient and user-friendly."
               ghLink="https://github.com/Prabakara-N/react-expense-tracker"
               demoLink="https://pk-react-expense-tracker.netlify.app/"
             />
@@ -72,16 +84,6 @@ const Projects = () => {
               description="This React.js weather app allows users to quickly retrieve weather data and current time and date information for any location by entering a city name. The app utilizes APIs and the useState hook to manage state within the application, and includes features such as input validation and loading animation to enhance user experience. Weather data is displayed in a separate component with weather icons. Overall, it's a well-designed and user-friendly app that demonstrates the power of modern web development frameworks."
               ghLink="https://github.com/Prabakara-N/react-weather-api"
               demoLink="https://pk-react-weather-api.netlify.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Expense_Tracker}
-              isBlog={false}
-              title="Solidity Kickstart"
-              description="Solidity Kickstart is a Web 3 project that utilizes an Ethereum smart contract created using Solidity. It includes all the functionality of a kickstart platform, enabling users to interact with their Metamask wallet, transfer Ethereum and participate in kickstart campaigns."
-              ghLink="https://github.com/19sajib/Solidity-Kickstart"
             />
           </Col>
         </Row>
